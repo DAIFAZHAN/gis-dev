250096a2c91fa16819d0c2e3fd92890ff73dffe3
daifzh <daifzh@163.com>
2023-04-06 01:22:39 +0800

    feat: 【瓦片风】加载图片和文本

--- src/components/mapbox/MapboxGL.vue
+++ src/components/mapbox/MapboxGL.vue
@@ -6,12 +6,14 @@
 import mapboxgl from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
 import { onMounted, ref } from 'vue'
+import { geojsonArea, images, boys } from './data/gisData'
 const mapEl = ref(null)
 
 const initOption = {
   style: {
     version: 8,
     id: '43f36e14-e3f5-43c1-84c0-50a9c80dc5c7',
+    glyphs: 'mapbox://fonts/mapbox/{fontstack}/{range}.pbf', // 解决名字图层报错
     sources: {
       'tdt-vec': {
         type: 'raster',
@@ -63,7 +65,7 @@ onMounted(() => {
   map.on('style.load', () => {
     map.addSource('geojson-area-source', {
       type: 'geojson',
-      // data: geojsonArea // 你得到的geojson
+      data: geojsonArea
     })
     map.addLayer({
       id: 'geojson-area-layer',
@@ -73,6 +75,59 @@ onMounted(() => {
       paint: {
         'fill-color': 'red',
         'fill-opacity': 0.5
+      }
+    })
+  })
+
+  // 加载图标和文本
+  const loadImages = async (imgs) => {
+    await Promise.all(
+      Object.entries(imgs).map(
+        ([key, url]) =>
+          new Promise((resolve) => {
+            map.loadImage(url, (error, res) => {
+              if (error) throw error
+              map.addImage(key, res)
+              resolve(res)
+            })
+          })
+      )
+    )
+  }
+  map.on('style.load', async () => {
+    // 加载图片
+    await loadImages(images)
+    // 添加位置资源
+    map.addSource('boys-source', {
+      type: 'geojson',
+      data: boys
+    })
+    // 添加ICON图层
+    map.addLayer({
+      id: 'boys-icon-layer',
+      type: 'symbol',
+      source: 'boys-source',
+      layout: {
+        'icon-image': '{icon}',
+        'icon-size': 0.2,
+        'icon-anchor': 'center',
+        'icon-rotation-alignment': 'viewport',
+        'icon-allow-overlap': true
+      }
+    })
+    // 添加名字图层
+    map.addLayer({
+      id: 'boys-name-layer',
+      type: 'symbol',
+      source: 'boys-source',
+      layout: {
+        'text-field': '{name}',
+        'text-size': 14,
+        'text-offset': [0, 2.4], // 名字要设置偏移，避免被头像挡住
+        'text-allow-overlap': true
+      },
+      paint: {
+        'text-color': 'white'
       }
     })
   })
